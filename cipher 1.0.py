ab12556359aaaf984c10ed87276af84c3b6831ff
rafiqi31 <75363348+rafiqi31@users.noreply.github.com>
2020-12-02 16:35:52 +0700

    Update cipher 1.0.py

--- cipher 1.0.py
+++ cipher 1.0.py
@@ -1,3 +1,7 @@
+------------------------
+|| Caesar Chipper 1.0 ||
+------------------------
+
 print("Pilih mode:")
 print("1 encrypt")
 print("2 decrypt")
@@ -24,3 +28,60 @@ for symbol in message:
     else:
         translated += symbol
 print(translated)
+
+
+------------------------
+|| Caesar Chipper 2.0 ||
+------------------------
+
+import string
+abjad = string.printable
+
+def enkrip (pesan):
+    global abjad
+
+    key = int (input ('Masukkan key : '))
+    cipher= ''
+    for i in pesan :
+        if i in abjad:
+            k = abjad.find(i)
+            k = (k+key)%100
+            cipher = cipher + abjad[k]
+        else:
+            cipher = cipher + i
+
+    return cipher
+
+def dekrip (cipher):
+    global abjad
+
+    key = int (input ('Masukkan key : '))
+    pesan= ''
+    for i in cipher :
+        if i in abjad:
+            k = abjad.find(i)
+            k = (k-key)%100
+            pesan = pesan + abjad[k]
+        else:
+            pesan = pesan + i
+
+    return pesan
+
+if __name__ == '__main__':
+    print('---------------------')
+    print('---------------------')
+    print('By : Kelompok 5')
+    print('Afnan, Usu, M. Rafiqi')
+    print('---------------------')
+    print('---------------------')
+    pilihan = int (input ('1. Enkripsi\n2. Dekripsi\n----------\nPilih mode :'))
+
+   
+    if pilihan == 1:
+        pesan = input ('Masukkan pesan (Plaintext) : ')
+        print (enkrip(pesan))
+    elif pilihan ==2:
+        cipher = input ('Masukkan pesan (CipherText) : ')
+        print (dekrip(cipher))
+    else:
+        print ('Masukkan pilihan 1 atau 2 !!')
